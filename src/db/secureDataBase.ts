8c747e3f6400cdb20d01c8dd7634775b5f961f1f
abhishek.mishra <abhishek.mishra@esparkbizmail.com>
2025-12-09 16:42:29 +0530

    (MA) :lint fix

--- src/db/secureDataBase.ts
+++ src/db/secureDataBase.ts
@@ -3,7 +3,7 @@ import { IDBPDatabase, openDB } from 'idb';
 // ===== DB CONFIG =====
 const VITE_IND_DB_NAME = import.meta.env.VITE_DB_NAME || 'nurse-appointment';
 const DB_VERSION = 2;
-const DEVICE_KEY_ID = import.meta.env.VITE_DEVICE_KEY_ID || 'device-key';
+// const DEVICE_KEY_ID = import.meta.env.VITE_DEVICE_KEY_ID || 'device-key';
 const VITE_IND_DB_TABLE = import.meta.env.VITE_IND_DB_TABLE || 'visits';
 type DB = IDBPDatabase;
 // Text helpers
@@ -324,7 +324,6 @@ class SecureDB {
     for (const item of items) {
       try {
         const enc = await this.encrypt<T>(item); // OK here
-        console.log({ enc });
         encryptedItems.push(enc);
       } catch (err) {
         console.error('Encryption failed for item:', item, err);
